ee5933a5c8069bec114f914ea14c6e3aa0f60fc2
sash9696 <sahilchopra838@gmail.com>
2022-12-03 12:40:30 +0530

    MINOR TEXT CHANGES

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Add <code>src/App.js</code> and save to reload the page.
+          EDIT <code>src/App.js</code> and save to reload the page.
         </p>
         <a
           className="App-link"
